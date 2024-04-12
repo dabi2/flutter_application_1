389ed4640e0e9bafe6c4d3143d7fa8b70a14df31
dabi2 <dabiangjalong3832gmail.com>
2024-04-12 11:28:18 +0530

    new update on auth

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Loginpage(),
+      home: const LogIn(),
     );
   }
 }
