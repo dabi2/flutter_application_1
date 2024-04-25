1bafeba15fa2d6d3178a080c48009b4b526c404e
dabi2 <dabiangjalong3832gmail.com>
2024-04-25 16:25:55 +0530

    ll

--- lib/razor_pay/razorpay.dart
+++ lib/razor_pay/razorpay.dart
@@ -1,29 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+
 import 'package:razorpay_flutter/razorpay_flutter.dart';
 
 class RazorpayScreen extends StatefulWidget {
-  const RazorpayScreen({super.key});
+  const RazorpayScreen({Key? key}) : super(key: key);
 
   @override
   State<RazorpayScreen> createState() => _RazorpayScreenState();
-
-
-  // void clear() {}
-
-  // void open(Map<String, Object> options) {}
 }
 
 class _RazorpayScreenState extends State<RazorpayScreen> {
   late Razorpay _razorpay;
   final TextEditingController amtController = TextEditingController();
-  void opencheck(amount) async {
-    amount = amount * 100;
+
+  void openCheck(double amount) async {
+    int amountInPaise = (amount * 100).toInt();
     var options = {
       'key': 'rzp_test_grI3dndnrMprjQ',
-      'amount': amount,
-      'name': 'Geeks for geeks',
-      'prefil': {'contact': '123456789', 'email': ' test@gmail.com'},
+      'amount': amountInPaise,
+      'name': 'Geeks for Geeks',
+      'prefill': {'contact': '123456789', 'email': 'test@gmail.com'},
       'external': {
         'wallets': ['paytm']
       }
@@ -31,25 +28,25 @@ class _RazorpayScreenState extends State<RazorpayScreen> {
     try {
       _razorpay.open(options);
     } catch (e) {
-      debugPrint("error : e ");
+      debugPrint("error: $e");
     }
   }
 
   void handlePaymentSuccess(PaymentSuccessResponse response) {
     Fluttertoast.showToast(
-        msg: "Payment successfull" + response.paymentId!,
+        msg: "Payment successful" + response.paymentId!,
         toastLength: Toast.LENGTH_SHORT);
   }
 
   void handlePaymentError(PaymentFailureResponse response) {
     Fluttertoast.showToast(
-        msg: "Payment fail  " + response.message!,
+        msg: "Payment failed: " + response.message!,
         toastLength: Toast.LENGTH_SHORT);
   }
 
-  void handleexternalWallet(ExternalWalletResponse response) {
+  void handleExternalWallet(ExternalWalletResponse response) {
     Fluttertoast.showToast(
-        msg: "external Wallet " + response.walletName!,
+        msg: "External Wallet: " + response.walletName!,
         toastLength: Toast.LENGTH_SHORT);
   }
 
@@ -65,7 +62,7 @@ class _RazorpayScreenState extends State<RazorpayScreen> {
     _razorpay = Razorpay();
     _razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, handlePaymentSuccess);
     _razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, handlePaymentError);
-    _razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, handleexternalWallet);
+    _razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, handleExternalWallet);
   }
 
   @override
@@ -73,43 +70,39 @@ class _RazorpayScreenState extends State<RazorpayScreen> {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
-        children: [
-          Container(
-            child: Column(
-              children: [
-                TextFormField(
-                  cursorColor: Colors.black,
-                  decoration: InputDecoration(
-                    labelText: "Enter Amount",
-                    labelStyle: TextStyle(fontSize: 20, color: Colors.white),
-                  ),
-                  controller: amtController,
-                  validator: (value){
-                    if(value == null || value.isEmpty){
-                      return 'Please enter Amount to be paid';
-                    }
-                    return null;
-                  },
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      if(amtController.text.toString().isEmpty){
-                        setState(() {
-                          int amount = int.parse(amtController.text.toString());
-                          opencheck(amount);
-                        });
+          children: [
+            Container(
+              child: Column(
+                children: [
+                  TextFormField(
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                      labelText: "Enter Amount",
+                      labelStyle: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                    controller: amtController,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter Amount to be paid';
                       }
-                      
+                      return null;
                     },
-                    child: Text("Make Payment")),
-
-                   
-              ],
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (amtController.text.isNotEmpty) {
+                        double amount = double.parse(amtController.text);
+                        openCheck(amount);
+                      }
+                    },
+                    child: Text("Make Payment"),
+                  ),
+                ],
+              ),
             ),
-          )
-        ],
+          ],
+        ),
       ),
-      )
     );
   }
 }
